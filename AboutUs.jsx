fceb9a14e2b62bf41cf8e72854f06c32b3c4a6f1
Pujaranimahanta <2301104350me@gcekjr.ac.in>
2026-01-28 17:04:09 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+const AboutUs = () => (
+<div className="about">
+<h2>About Paradise Nursery</h2>
+<p>
+Paradise Nursery is your one-stop destination for premium indoor plants.
+We believe plants bring peace, positivity, and beauty into everyday life.
+</p>
+</div>
+);
+
+
+export default AboutUs;
